2a7467c4c5e3cc065f707982cf7631321ba8e4b5
nathprabir1983 <114485394+nathprabir1983@users.noreply.github.com>
2026-04-20 21:29:08 +0530

    run command changes
    run command changes

--- main.py
+++ main.py
@@ -68,5 +68,5 @@ def delete_item(item_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Item not found")
     return {"message": "Item deleted"}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+#if __name__ == "__main__":
+#    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
